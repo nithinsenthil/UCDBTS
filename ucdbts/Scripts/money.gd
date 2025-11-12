0e0c2b76bdbfff816aa8f3cce7123272f53037dd
Ria Rajput <riarajput@ucdavis.edu>
2025-11-12 14:42:09 -0800

    added money class and functions to buy and sell goods. Also added a display to see money visually on the screen

---
+++ ucdbts/Scripts/money.gd
@@ -0,0 +1,27 @@
+class_name Money
+extends Node
+
+var _total_funds = 0
+
+@onready var money_label: Label = $Label
+
+func get_total_funds():
+	return _total_funds
+	
+func buy_item(item_price):
+	var remaining_funds = _total_funds - item_price
+	if remaining_funds < 0:
+		return false
+	else:
+		_total_funds -= item_price
+		return true
+
+func sell_item(item_price):
+	_total_funds += item_price
+	
+func update_label():
+	money_label.text = "$" + str(_total_funds)
+
+func _ready():
+	# Initialize the label when the scene starts
+	update_label()
