8c26b0a5c4ebe4a8b7a206b715274d1d92794a37
Wesley Clark <wjclark@ucdavis.edu>
2025-11-24 17:12:27 -0800

    fix: disable left-over controls for debugging camera

--- ucdbts/Scripts/Debugging/debug_label.gd
+++ ucdbts/Scripts/Debugging/debug_label.gd
@@ -13,11 +13,11 @@ func _process(_delta: float) -> void:
 			player.global_position.y, camera.global_position.x, camera.global_position.y, 
 			player.Facing.keys()[player._facing], camera.offset.x, camera.offset.y]
 			
-	if Input.is_action_just_pressed("CameraControlsOn"):
-		signals.toggle_camera_controls.emit(true)
-	if Input.is_action_just_pressed("CameraControlsOff"):
-		signals.toggle_camera_controls.emit(false)
-	if Input.is_action_just_pressed("BikeMinigameStart"):
-		signals.bike_minigame_start.emit()
-	if Input.is_action_just_pressed("BikeMinigameEnd"):
-		signals.bike_minigame_end.emit()
+	#if Input.is_action_just_pressed("CameraControlsOn"):
+		#signals.toggle_camera_controls.emit(true)
+	#if Input.is_action_just_pressed("CameraControlsOff"):
+		#signals.toggle_camera_controls.emit(false)
+	#if Input.is_action_just_pressed("BikeMinigameStart"):
+		#signals.bike_minigame_start.emit()
+	#if Input.is_action_just_pressed("BikeMinigameEnd"):
+		#signals.bike_minigame_end.emit()
