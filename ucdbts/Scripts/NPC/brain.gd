b6d3f58049c84d4378b9f279131830040b77f646
jacobtkeio <jacobtkeio@gmail.com>
2025-11-30 01:48:34 +0000

    Add npc_factory with stat randomization

--- ucdbts/Scripts/NPC/brain.gd
+++ ucdbts/Scripts/NPC/brain.gd
@@ -1,6 +1,11 @@
 class_name Brain
 extends Node
 
+const MAX_MAX_SUS: float = 100.0
+const MIN_MAX_SUS: float = 50.0
+const MEAN_MAX_SUS: float = 100.0
+const STDEV_MAX_SUS: float = 10.0
+
 var suspicion : float = 0.0
 var _maximum_suspicion : float = INF
 
