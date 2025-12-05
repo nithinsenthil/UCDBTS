4a3d0effc521deecdefa10bff604ac3d0db6c2e2
Ria Rajput <riarajput@ucdavis.edu>
2025-12-05 12:14:15 -0800

    Added credit screen that scrolls automatically

---
+++ ucdbts/Scripts/credits.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var timer: Timer
+@onready var audio_manager: AudioManager = $AudioManager
+@onready var player: AudioStreamPlayer2D = $AudioManager/Background/TitleScreen
+
+func _ready() -> void:
+	timer = Timer.new()
+	timer.wait_time = 0.5
+	timer.one_shot = true
+	timer.timeout.connect(_check_music)
+	add_child(timer)
+	timer.start()
+	player.playing = false
+
+
+func _check_music():
+	#var audio_manager: AudioManager = $AudioManager
+	#var player: AudioStreamPlayer2D = $AudioManager/Background/TitleScreen
+	if player == null:
+		return
+	elif !player.playing:
+		audio_manager.fade_music_in(player, -5)
+	
+	timer.start()
