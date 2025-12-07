24b8415f4567950cdb117339dab4bfa503072009
Naomi Zhao <naomitan.zhao@gmail.com>
2025-12-07 00:34:22 -0800

    Title screen visual update

---
+++ ucdbts/Scripts/title_text.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	animation_player.play("hover")
