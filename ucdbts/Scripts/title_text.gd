68718f014f27a1b9ad258fef860eda7150d0c4f4
Naomi Zhao <naomitan.zhao@gmail.com>
2025-12-07 00:39:08 -0800

    Fix title_text.gd

--- ucdbts/Scripts/title_text.gd
+++ ucdbts/Scripts/title_text.gd
@@ -1,12 +1,6 @@
-extends Node
+extends TextureRect
 
 @onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	animation_player.play("hover")
