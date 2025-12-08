73666e7d02dc425ba8144981f17dac3be645914c
Naomi Zhao <naomitan.zhao@gmail.com>
2025-12-07 17:21:41 -0800

    New HUD template

--- ucdbts/Scripts/pockets.gd
+++ ucdbts/Scripts/pockets.gd
@@ -12,4 +12,4 @@ func _ready() -> void:
 func update_label() -> void:
 	#pockets_label.text = "Pockets: %d/%d\nTotal held: $%d" % [ResourceManager.get_num_held_bikes(), ResourceManager.get_max_pocket_size(), 
 			#ResourceManager.get_pocket_value()]
-	pockets_label.text = "Total Worth: $ %d" % [ResourceManager.get_pocket_value()]
+	pockets_label.text = "Bikes: $%d" % [ResourceManager.get_pocket_value()]
