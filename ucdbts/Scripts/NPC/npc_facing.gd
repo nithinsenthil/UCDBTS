3195c84f1e9661bd8da7d1036a86ea080fedcaf6
Naomi Zhao <naomitan.zhao@gmail.com>
2025-12-07 23:29:59 -0800

    Use AnimationPlayer for NPCs

--- ucdbts/Scripts/NPC/npc_facing.gd
+++ ucdbts/Scripts/NPC/npc_facing.gd
@@ -1,6 +1,32 @@
 class_name NPCFacing
-extends CharacterFacing
+extends Sprite2D
+
+@onready var animation_player = $AnimationPlayer
+
+const direction_strings = {
+	Character.Facing.DEFAULT: "front",
+	Character.Facing.LEFT: "side",
+	Character.Facing.RIGHT: "side",
+	Character.Facing.UP: "back",
+	Character.Facing.UP_LEFT: "side",
+	Character.Facing.UP_RIGHT: "side",
+	Character.Facing.DOWN: "front",
+	Character.Facing.DOWN_LEFT: "side",
+	Character.Facing.DOWN_RIGHT: "side",
+}
+
+const action_strings = {
+	Character.Action.IDLE: "idle",
+	Character.Action.WALK: "walk",
+	Character.Action.RUN: "run",
+}
 
 
-func _ready():
-	pass # Do not hook up signals
+func _on_sprite_change(facing: Character.Facing, action: Character.Action) -> void:
+	if (facing == Character.Facing.RIGHT
+			or facing == Character.Facing.UP_RIGHT
+			or facing == Character.Facing.DOWN_RIGHT):
+		flip_h = true
+	else:
+		flip_h = false
+	animation_player.play(action_strings[action] + "_" + direction_strings[facing])
